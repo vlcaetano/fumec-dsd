4a4be537101e0a1d6b7fd474cf53c27508762849
Vitor Caetano <vitorlc95@gmail.com>
2021-08-28 19:45:53 -0300

    Exercício 1.4 - melhorias

--- 01-Exercicios-Garg-1_4/src/Main.java
+++ 01-Exercicios-Garg-1_4/src/Main.java
@@ -1,28 +1,30 @@
+import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int[] vetor = {3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50};
-
-		System.out.println("Vetor desordenado: ");
-		for (int i : vetor) {
-			System.out.println(i + " ");
-		}
+		int[] vetor = new int[30];
+		Random random = new Random();
 		
+		System.out.println("Vetor desordenado: ");
+		for (int i = 0; i < vetor.length; i++) {
+			vetor[i] = random.nextInt(100);
+			System.out.print(vetor[i] + " ");
+		}
+
 		MergeSortThread t = new MergeSortThread(0, vetor.length, vetor);
 		t.start();
-		
+
 		try {
-			t.join ();
-		} catch (InterruptedException e){
-			System.out.println("Erro ");
+			t.join();
+		} catch (InterruptedException e) {
+			System.out.println("Erro");
 		}
-		
-		System.out.println("\n----------------------------------------");
+
+		System.out.println();
 		System.out.println("Vetor ordenado: ");
 		for (int i : vetor) {
-			System.out.println(i + " ");
+			System.out.print(i + " ");
 		}
 	}
-
 }
--- 01-Exercicios-Garg-1_4/src/MergeSortThread.java
+++ 01-Exercicios-Garg-1_4/src/MergeSortThread.java
@@ -1,10 +1,10 @@
 
 public class MergeSortThread extends Thread {
-	
+
 	private int inicio;
 	private int fim;
 	private int[] vetor;
-	
+
 	public MergeSortThread(int inicio, int fim, int[] vetor) {
 		this.inicio = inicio;
 		this.fim = fim;
@@ -22,27 +22,27 @@ public class MergeSortThread extends Thread {
 
 			mst1.start();
 			mst2.start();
-			
+
 			try {
-				mst1.join ();
-				mst2.join ();
-			} catch (InterruptedException e){
+				mst1.join();
+				mst2.join();
+			} catch (InterruptedException e) {
 				System.out.println("Erro");
 			}
-			
+
 			intercala(vetor, inicio, meio, fim);
 		}
 	}
-	
+
 	private static void intercala(int[] vetor, int inicio, int meio, int fim) {
 		int[] novoVetor = new int[fim - inicio];
 		int i = inicio;
 		int m = meio;
-		
+
 		int pos = 0;
-		
+
 		while (i < meio && m < fim) {
-			
+
 			if (vetor[i] <= vetor[m]) {
 				novoVetor[pos] = vetor[i];
 				pos = pos + 1;
@@ -53,19 +53,19 @@ public class MergeSortThread extends Thread {
 				m = m + 1;
 			}
 		}
-		
+
 		while (i < meio) {
 			novoVetor[pos] = vetor[i];
 			pos = pos + 1;
 			i = i + 1;
 		}
-		
+
 		while (m < fim) {
 			novoVetor[pos] = vetor[m];
 			pos = pos + 1;
 			m = m + 1;
 		}
-		
+
 		for (pos = 0, i = inicio; i < fim; i++, pos++) {
 			vetor[i] = novoVetor[pos];
 		}
